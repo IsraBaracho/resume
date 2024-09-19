032163a11da4d7a6222c65033da5a9f16925d815
Israel Baracho <israel@feevale.br>
2024-09-19 15:42:06 -0300

    Commit 001

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,19 @@ module.exports = {
     title: `IsraelResume`,
     siteUrl: `https://www.yourdomain.tld`
   },
-  plugins: ["gatsby-plugin-sitemap"]
+  plugins: [
+    {
+      resolve: `gatsby-omni-font-loader`,
+      options: {
+        enableListener: true,
+        preconnect: [`https://fonts.googleapis.com`, `https://fonts.gstatic.com`],
+        web: [
+          {
+            name: `Jersey`,
+            file: 'https://fonts.googleapis.com/css2?family=Jersey+25&display=swap',
+          },
+        ],
+      },
+    }
+  ]
 };
